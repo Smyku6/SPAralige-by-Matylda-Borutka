d44ed6223f8188b8db7fd2f280487f341bfc9814
Smyku6 <smyku6@gmail.com>
2022-04-03 17:45:55 +0200

    pis jou2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 function App() {
-
-  function jajo(): string {
-    return 'dfokls';
-  }
-
   return (
     <div className="App">
       <h1>SPAralige</h1>
       <h1>SPAralige by Matylda Borutka </h1>
-      {jajo()}
     </div>
   );
 }
